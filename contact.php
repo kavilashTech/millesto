a3c6f7a407d5cb2ca0bc46c572d0996115cef594
Sampath <kavilashtech@gmail.com>
2024-07-30 16:27:59 +0530

    map updated

--- contact.php
+++ contact.php
@@ -22,17 +22,21 @@ include "includes/header.php";
                     <div class="row g-4">
                         <div class="col-12">
                             <div class="text-center mx-auto" style="max-width: 700px;">
-                                <h1 class="text-primary">Get in touch</h1>
+                                <h1 class="text-primary pt-3">Get in touch</h1>
                                 <p class="mb-4">We love to talk. Ask your questions, doubts or just have a chat. Let us know your thoughts so that we can provide a better product everytime.</p>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <div class="h-100 rounded">
                                 <iframe class="rounded w-100" 
-                                style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd" 
+                                
+                                style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1690.3597536055295!2d150.9474130387769!3d-33.78766626411771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b1298795bacfda7%3A0xef39c5d7ac270cbc!2sToongabbie%20NSW%202146%2C%20Australia!5e1!3m2!1sen!2sin!4v1722336973565!5m2!1sen!2sin" 
                                 loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
+
+
+                        <!-- <iframe  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
                         <div class="col-lg-7">
                             <form action="" class="">
                                 <input type="text" class="w-100 form-control border-0 py-3 mb-4" placeholder="Your Name">
@@ -46,7 +50,7 @@ include "includes/header.php";
                                 <i class="fas fa-map-marker-alt fa-2x text-primary me-4"></i>
                                 <div>
                                     <h4>Address</h4>
-                                    <p class="mb-2">123 Street New York.USA</p>
+                                    <p class="mb-2">10, Junia Ave, Toongabbie, NSW 2146, Australia</p>
                                 </div>
                             </div>
                             <div class="d-flex p-4 rounded mb-4 bg-white">
@@ -60,7 +64,7 @@ include "includes/header.php";
                                 <i class="fa fa-phone-alt fa-2x text-primary me-4"></i>
                                 <div>
                                     <h4>Telephone</h4>
-                                    <p class="mb-2">(+012) 3456 7890</p>
+                                    <p class="mb-2">+61 401 472 787</p>
                                 </div>
                             </div>
                         </div>
